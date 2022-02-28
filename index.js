1cfcdb8f6b0e27ff0a511123917815e80b94328c
Monet Man <88108211+jpcisneros411@users.noreply.github.com>
2022-02-28 13:27:37 -0600

    setting up inquirer

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const prompts = require('inquirer');
+var inquirer = require('inquirer');
+//const prompts = require('inquirer');
 
 function userInput(){
-    
+//this is going to ask the user questions like "what does this do or that do title etc."
+    return 
+    inquirer.prompt([
+
+    ])
 }
+
+async function init(){
+    //
+}
+
+init();
